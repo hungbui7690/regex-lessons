c9141396cb915ad2a104327835e230fa5f630231
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 21:31:43 -0600

    Caret P1

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 /*
-  
+  Caret P1
+  - Anchors have special meaning in regular expressions. They do not match any character. Instead, they match a position before or after characters:
+    + ^ – The caret anchor matches the beginning of the text.
+    + $ – The dollar anchor matches the end of the text.
+
+
 */
+
+let str = 'JavaScript'
+console.log(/^J/.test(str)) // begin with J > true
