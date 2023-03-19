ba3132d909671def1f37089e5ade496c86782022
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 20:24:58 -0600

    Inverse Classes P1

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
 /*
-  Intro
-  - example: 
-    > +1-(408)-555-0105
-  - we want to turn this into 
-    > 14085550105
+  Inverse Classes P1
+  - \D is the inverse of \d
+    > \D – matches any character except a digit e.g., a letter.
+    > \S – matches any character except a whitespace e.g., a letter
+    > \W – matches any character except a word character e.g., non-Latin letter or space.
 
-///////////////////////////////
-
-  Whitespace 
-  - \s
-
-  Word Character 
-  - \w 
-    > [A-Za-z0-9_]
 
 
 */
 
-let str = 'ES6 Tutorial'
-let re = /ES\d/g // ES followed by a digit like ES6
+let phone = '+1-(408)-555-0105'
+let re = /\d/g
 
-console.log(str.match(re)) // ['ES6']
+console.log(phone.match(re).join('')) // 14085550105
