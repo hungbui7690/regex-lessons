d3fb5d0c7c7dabf4dd237db08b001665f073eee7
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 21:47:55 -0600

    Exact count {n}

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 /*
-  
+  Quantity
+  - Exact count {n}
+    > A number in curly braces {n}is the simplest quantifier. When you append it to a character or character class, it specifies how many characters or character classes you want to match.
+
 */
+
+// For example, the regular expression /\d{4}/ matches a four-digit number. It is the same as /\d\d\d\d/:
+let str = 'ECMAScript 2020'
+let re = /\d{4}/
+
+let result = str.match(re)
+
+console.log(result) // ["2020"]
