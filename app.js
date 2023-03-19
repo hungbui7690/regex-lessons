ca5c2fc3a3fb479cc1d8490ce7df3599dcb731c4
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 23:10:03 -0600

    Negative Lookbehind

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
 /*
-  Lookbehind
-  - In regular expressions, a lookbehind matches an element if there is another specific element before it. A lookbehind has the following syntax:
-    >   (?<=Y)X
+  Negative Lookbehind
+  - To negate a lookbehind, you use a negative lookbehind with the following syntax:
+    >   (?<!Y)X
   
-  - In this syntax, the pattern match X if there is Y before it.
-
-  - For example, suppose you want to match the number 900 not the number 1 in the following string:
-    >   '1 computer costs $900'
-
-  - To do it, you use a lookahead in the regular expression as follows:
-    >   /(?<=\$)\d+/
-
-  - In this regular expression:
-    + The (?<=\$) matches an element if there is a literal string $ before it. Because $ is a special character in the regex, we need to use the backslash \ to escape it. By doing this, the regex engine treats \$ as a literal character $.
-    + The \d+ matches one or more digits.
+  - In this syntax, the regex engine matches X if there is no Y before it. 
 
 */
 
-// The following example illustrates how to use a lookbehind in a regular expression to match a number that has the $ sign before it:
+// The following example uses a regular expression with a negative lookbehind to match a number that doesn’t have the $ letter before it:
 const s = '1 computer costs $900'
-const pattern = /(?<=\$)\d+/
+const pattern = /(?<!\$)\d+/
 
 const match = s.match(pattern)
-console.log(match) // [ '900', index: 18, input: '1 computer costs $900', groups: undefined ]
+console.log(match) // 1
+
+////////////////////////////////////
+
+// A lookbehind (?<!Y)X matches X only if is is preceded by Y.
+// A negative lookbehind (?<!Y)X matches X only if it is not preceded by Y.
