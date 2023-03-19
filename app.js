0a1d4a508cd4d6a4a102eb905adc7f17b1c4468c
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 21:45:51 -0600

    Word Boundaries P4

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 /*
-  Word Boundaries P3
-  - It’s possible to use the word boundary with digits.
+  Word Boundaries P4
+  - As you have seen so far, the patterns \d\d\d\d and \d\d has been used to match a four-digit or a two-digit number.
+
+  - It’ll be easier and more flexible if you use quantifiers that will be covered in the quantifiers tutorial. Basically, you can use \d{4} instead of \d\d\d\d, which is much shorter.
 
 */
 
-// For example, the regular expression \b\d\d\d\d\b matches a 4-digit number surrounded by characters different from \w:
-console.log('ES 2015'.match(/\b\d\d\d\d\b/)) // ["2015"]
+// The following example uses the word boundary to find the time that has the format hh:mm e.g., 09:15:
+let str = 'I start coding JS at 05:30 AM'
+let re = /\b\d\d:\d\d\b/
+let result = str.match(re)
+
+console.log(result) // ["05:30"]
