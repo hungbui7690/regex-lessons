21d8f0e79afa30fb0dcf30510e877b24bc9cd52e
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 22:47:08 -0600

    Capturing groups P2

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 /*
-  Capturing groups P1
-  - Suppose you have the following path:
-    > resource/id
+  Capturing groups P2
+  - To get the id 10 from the path posts/10, you use a capturing group. To create a capturing group for a rule, you place that rule in parentheses like this:
+    > (rule)
 
-  - For example:
-    > posts/10
-
-  - In the path, the resource is posts and id is 10. To match the path, you use the following regular expression:
-    > /\w+\/\d+/
-
-  - In this pattern:
-    >   \w+ is a word character set with a quantifier (+) that matches one or more word characters.
-    >   / matches the forward slash (/). The backslash (\) escapes the forward slash,
-    >   \d+ is the combination of the digit character set and a quantfifer (+), which matches one or more digits.
+  - The following example creates a capturing group that captures the id value from the path posts/10:
+    > '\w+/(\d+)'
 
 */
 
-// The following uses the regular expression /\w+\/\d+/ pattern to match the string ‘posts/10':
+/////////////////////////////////
+
+// In this pattern, we place the rule \d+ inside the parentheses (). If you run the program with the new pattern, you’ll see that it displays one match:
 const path = 'posts/10'
-const pattern = /\w+\/\d+/
+const pattern = /\w+\/(\d+)/
 
 const match = path.match(pattern)
-console.log(match) // ['posts/10', index: 0, input: 'posts/10', groups: undefined]
+console.log(match) // ['posts/10', '10', index: 0, input: 'posts/10', groups: undefined]
+
+//////////////////////////////////
+
+// The match() method returns an array of matches. The first element is the whole match ('posts/10') while the second one ('10') is the value of the capturing group.
+
+// Note that the String.match() method will not return groups if you set the g flag e.g., /\w+\/(\d+)/g. If you use the g flag, you need to use the String.matchAll() method instead to get the groups.
