8f29b5228535f5b26786c75ee048b4af2dfb43b6
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 23:08:40 -0600

    Lookbehind

--- app.js
+++ app.js
@@ -1,35 +1,25 @@
 /*
-  Multiple Lookaheads
-  - It’s possible to have multiple lookaheads in a regular expression with the following syntax:
-    > X(?=Y)(?=Z)
-  - In this syntax, the regex engine performs the following steps:
-    > Search for X
-    > Check if Y is immediately after X, skip if it isn’t.
-    > Check if Z is also immediately after Y; skip if it isn’t.
-    > If both tests pass, return X as a match; otherwise, search for the next match.
-    (***) Therefore, the X(?=Y)(?=Z) matches X followed by Y and Z simultaneously.
+  Lookbehind
+  - In regular expressions, a lookbehind matches an element if there is another specific element before it. A lookbehind has the following syntax:
+    >   (?<=Y)X
+  
+  - In this syntax, the pattern match X if there is Y before it.
 
-//////////////////////////////////
+  - For example, suppose you want to match the number 900 not the number 1 in the following string:
+    >   '1 computer costs $900'
 
-  Regex negative lookaheads
-  - Suppose you want to match the number 1 but not the number 15 in the following string:
-    > const s = '1 car is 15 feet long';
+  - To do it, you use a lookahead in the regular expression as follows:
+    >   /(?<=\$)\d+/
 
-  - To do that, you use a negative lookahead. Here’s the syntax of the negative lookahead:
-    > X(?!Y)
-
-  - In this syntax, the regex engine matches X only if it is not followed by Y.
+  - In this regular expression:
+    + The (?<=\$) matches an element if there is a literal string $ before it. Because $ is a special character in the regex, we need to use the backslash \ to escape it. By doing this, the regex engine treats \$ as a literal character $.
+    + The \d+ matches one or more digits.
 
 */
 
-// The following example illustrates how to use a negative lookahead:
-const s = '1 car is 15 feet long'
-const pattern = /\d+(?!\s*feet)/
+// The following example illustrates how to use a lookbehind in a regular expression to match a number that has the $ sign before it:
+const s = '1 computer costs $900'
+const pattern = /(?<=\$)\d+/
 
 const match = s.match(pattern)
-console.log(match) // 1
-
-////////////////////////////////////////
-
-// A regex lookahead X(?=Y) matches X only if it is followed by Y.
-// A negative regex lookahead X(?!Y) matches X only if it is not followed by Y.
+console.log(match) // [ '900', index: 18, input: '1 computer costs $900', groups: undefined ]
