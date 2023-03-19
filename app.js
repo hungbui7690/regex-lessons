3a44989d8692ecf741d4d520b9984df060931141
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 20:06:43 -0600

    The Global Flag (g)

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 /*
-  The Ignore Flag (i) P2
+  The Global Flag (g)
+  - without global flag > return the first match
 
 */
 
-let regex = new RegExp('hi', 'i') // (***) alternative
+let message = 'Hi, are you there? hi, HI...'
+let regex = /hi/gi
 
-const result = regex.test('HI, John')
-console.log(result)
+let matches = []
+let match
+
+do {
+  match = regex.exec(message) // true/false
+  if (match) matches.push(match)
+} while (match !== null)
+
+console.log(matches) // returns array of arrays
