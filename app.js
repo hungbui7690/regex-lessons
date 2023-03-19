0ba7f02902c7f70c3665e8bf46d21bf117891ea8
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 22:58:06 -0600

    Named capturing groups P2

--- app.js
+++ app.js
@@ -1,22 +1,6 @@
 /*
-  Named capturing groups P1
-  - To access a subgroup in a match, you use an index. However, you may want to access a subgroup by a meaningful name to make it more convenient.
-
-  - To do that, you use the named capturing group to assign a name to a group. The following shows the syntax for assigning a name to a capturing group:
-    > (?<name>rule)
-    
-    > In this syntax:
-      + () indicates a capturing group.
-      + ?<name> specifies the name of the capturing group.
-      + rule is a rule in the pattern.
-
-
-  - For example, the following creates the names:
-    > /?<resource>\w+)\/(?<id>\d+/
-
-    > In this syntax:
-      + The resource is the name for the first capturing group
-      + The id is the name for the second capturing group.
+  Named capturing groups P2
+  - The match has a groups property that is an object. The match.groups object has properties whose names are the capturing group name and values are the capturing values. 
 
 */
 
@@ -24,4 +8,8 @@ const path = 'posts/10'
 const pattern = /(?<resource>\w+)\/(?<id>\d+)/
 
 const match = path.match(pattern)
-console.log(match) // (3) ['posts/10', 'posts', '10', index: 0, input: 'posts/10', groups: {…}]
+for (const name in match.groups) {
+  // resource posts
+  // id 10
+  console.log(name, match.groups[name])
+}
