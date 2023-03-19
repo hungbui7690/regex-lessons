e971d315fb2da49bef9cc6d96dc9125b18523607
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 20:26:43 -0600

    The dot (.) character class P1

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 /*
-  Inverse Classes P2
-  - \D is the inverse of \d
-    > \D – matches any character except a digit e.g., a letter.
-    > \S – matches any character except a whitespace e.g., a letter
-    > \W – matches any character except a word character e.g., non-Latin letter or space.
-
+  The dot (.) character class P1
+  - The dot (.) is a special character class that matches any character except a newline
 
 
 */
 
-let phone = '+1-(408)-555-0105'
-let re = /\D/g
-
-console.log(phone.replace(re, '')) // replace non-digit with ''
+let re = /E.6/
+console.log('ES6'.match(re)) // ['ES6', index: 0, input: 'ES6', groups: undefined]
