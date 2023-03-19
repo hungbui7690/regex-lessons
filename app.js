a12501c1dba3039aa47bf6296a9f17c6683d3921
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 21:43:32 -0600

    Word Boundaries P2

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 /*
-  Word Boundaries P1
-  - The (\b) is an anchor like the caret (^) and the dollar sign ($). It matches a position that is called a “word boundary”. The word boundary match is zero-length.
-
-  - The following three positions are qualified as word boundaries:
-    + Before the first character in a string if the first character is a word character.
-    + After the last character in a string if the last character is a word character.
-    + Between two characters in a string if one is a word character and the other is not.
-
-  - Simply put, the word boundary \b allows you to carry the match the whole word using a regular expression in the following form:
-    > \bword\b
+  Word Boundaries P2
 
 */
 
-console.log('Hello, JS!'.match(/\bJS\b/)) // ["JS"]
+// The following example returns 'JS' because 'Hello, JS!' matches the regular expression /\bJS\b/:
+console.log('Hello, JSscript!'.match(/\bJS\b/)) // null
+
+// Note that without \b, the /JS/ matches both 'Hello, JS' and 'Hello, JScript':
+console.log('Hello, JSscript!'.match(/JS/)) // ["JS"]
+console.log('Hello, JS!'.match(/JS/)) // ["JS"]
