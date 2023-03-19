a53985bb7aa02b6b4485219b2271c9f945ff4909
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 21:38:43 -0600

    The "m" Flag P2

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 /*
-  Multiline mode of anchors ^ and $: The "m" Flag P1
-  - The default of the anchor ^ or $ is the single-line mode. In the single-line mode, the anchor ^ and $ matches the beginning and the end of a string.
-
-  - To enable the multiline mode, you use m flag. In the multiline mode, the ^ or $ anchor matches the beginning or end of the string as well as the beginning or end of lines.
-
-
+  Multiline mode of anchors ^ and $: The "m" Flag P2
+  
 */
 
 let str = `1st line
 2nd line
 3rd line`
 
-let re = /^\d/g // start with digit > though we use global flag, it just return the first match
+let re = /^\d/gm // global + multi lines
 let matches = str.match(re)
 
-console.log(matches)
+console.log(matches) // ["1", "2", "3"]
