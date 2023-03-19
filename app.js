dd40b08af984a332bce3ae735fa11e16ccd33a76
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 20:28:01 -0600

    The dot (.) character class P3

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 /*
-  The dot (.) character class P2
-  - The dot (.) is a special character class that matches any character except a newline
+  The dot (.) character class P3
+  - If you want to use the dot (.) character class to match any character including the newline, you can use the s flag
 
 
 */
 
-let re = /ES.6/
-console.log('ES\n6'.match(re)) // null
+let re = /ES.6/s
+console.log('ES\n6'.match(re)) // ['ES\n6', index: 0, input: 'ES\n6', groups: undefined]
