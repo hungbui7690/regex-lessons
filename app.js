0f0fd74b64c1fbaacd5a6c4bb555dadf89857b23
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 21:37:51 -0600

    The "m" Flag P1

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 /*
-  Combine Both Anchors P2
-  - You will often need to use anchors ^ and $ to check if a string fully matches a pattern.
+  Multiline mode of anchors ^ and $: The "m" Flag P1
+  - The default of the anchor ^ or $ is the single-line mode. In the single-line mode, the anchor ^ and $ matches the beginning and the end of a string.
+
+  - To enable the multiline mode, you use m flag. In the multiline mode, the ^ or $ anchor matches the beginning or end of the string as well as the beginning or end of lines.
 
 
 */
 
-let valid = /^\d\d:\d\d$/.test('12:105')
-console.log(valid) // false
+let str = `1st line
+2nd line
+3rd line`
+
+let re = /^\d/g // start with digit > though we use global flag, it just return the first match
+let matches = str.match(re)
+
+console.log(matches)
