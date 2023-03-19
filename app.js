c3613f85dee8b84ba46640170df3aee08e43ce88
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 21:35:45 -0600

    Combine Both Anchors P2

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 /*
-  Combine Both Anchors P1
+  Combine Both Anchors P2
   - You will often need to use anchors ^ and $ to check if a string fully matches a pattern.
 
 
 */
 
-// The following example checks if an input string matches a time format hh:mm like 12:05:
-let isValid = /^\d\d:\d\d$/.test('12:05')
-console.log(isValid) // true
+let valid = /^\d\d:\d\d$/.test('12:105')
+console.log(valid) // false
