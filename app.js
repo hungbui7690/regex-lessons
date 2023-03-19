a34c3704fa91138ccddbad6471716cca63160444
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 20:18:24 -0600

    Digit Character P1

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 /*
-  
+  Intro
+  - example: 
+    > +1-(408)-555-0105
+  - we want to turn this into 
+    > 14085550105
+
+///////////////////////////////
+
+  Digit Character P1
+  - \d
+
 */
+
+let phone = '+1-(408)-555-0105'
+let re = /\d/g
+
+console.log(phone.match(re)) // ['1', '4', '0', '8', '5', '5', '5', '0', '1', '0', '5']
