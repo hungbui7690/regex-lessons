8ca64f6038237dab46c8386b233c6aad7ff7f4ac
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 20:27:14 -0600

    The dot (.) character class P2

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 /*
-  The dot (.) character class P1
+  The dot (.) character class P2
   - The dot (.) is a special character class that matches any character except a newline
 
 
 */
 
-let re = /E.6/
-console.log('ES6'.match(re)) // ['ES6', index: 0, input: 'ES6', groups: undefined]
+let re = /ES.6/
+console.log('ES\n6'.match(re)) // null
